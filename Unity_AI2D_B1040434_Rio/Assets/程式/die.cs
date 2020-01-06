9f918fb887489df99c6f42c6cbde0de994518c11
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2020-01-06 09:06:52 +0800

    end

---
+++ Unity_AI2D_B1040434_Rio/Assets/程式/die.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class die : MonoBehaviour
+{
+    public int damage = 100;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            collision.gameObject.GetComponent<play>().Damage(damage);
+        }
+    }
+}
